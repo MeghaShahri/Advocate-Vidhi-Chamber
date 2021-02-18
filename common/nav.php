b1d04142bf8b91d430d07ccdda0c69f88801bb10
MeghaShahri <meghashahri@gmail.com>
2021-02-18 19:54:52 +0530

    5

---
+++ common/nav.php
@@ -0,0 +1,69 @@
+<div class="wrapper">
+            <!-- Top Bar Start -->
+            <div class="top-bar">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <h1 style="font-size:40px;">Vidhi Chamber</h1>
+                                    <!-- <img src="img/logo.jpg" alt="Logo"> -->
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-9">
+                            <div class="top-bar-right">
+                                <div class="text">
+                                    <p>Mon: 11:00 am - 5:30 am, Tue: 11:00 am - 8:00 pm, Wed: 11:00 am - 8:00 pm,<br> Thurs: 11:00 am - 8:00 pm, Fri: 11:00 am - 8:00 pm, Sat: 11:00 am - 8:00 pm, Sun: 5:30 am - 8:00 pm</p>
+                                    
+                                </div>
+                                <div class="text">
+                                    <h3>+91 9599-000-555</h3>
+                                    <p>Call Us For Consultation</p>
+                                </div>
+                                <div class="social">
+                                    <a href=""><i class="fab fa-twitter"></i></a>
+                                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                    <a href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Top Bar End -->
+
+            <!-- Nav Bar Start -->
+            <div class="nav-bar">
+                <div class="container-fluid">
+                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                        <a href="#" class="navbar-brand">MENU</a>
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav mr-auto">
+                                <a href="index.php" class="nav-item nav-link active">Home</a>
+                                <a href="about.php" class="nav-item nav-link">About</a>
+                                <a href="service.php" class="nav-item nav-link">Practice Areas</a>
+                                <a href="team.php" class="nav-item nav-link">Our Team</a>
+                                <a href="portfolio.php" class="nav-item nav-link">Updates</a>
+                                <div class="nav-item dropdown">
+                                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
+                                    <div class="dropdown-menu">
+                                        <a href="blog.php" class="dropdown-item">Blog Page</a>
+                                        <a href="single.php" class="dropdown-item">Single Page</a>
+                                    </div>
+                                </div>
+                                <a href="contact.php" class="nav-item nav-link">Contact</a>
+                            </div>
+                            <div class="ml-auto">
+                                <a class="btn" href="https://htmlcodex.com/law-firm-website-template">Get Appointment</a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <!-- Nav Bar End -->
